7f724e5a821e742182ec94c57129ce14d363b912
NFLEagles500 <NFLEagles500@gmail.com>
2023-06-04 15:09:29 -0600

    Update main.py
    added an lcd display

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from lcd1602 import LCD
 import network
 import _thread
 from microdot import Microdot, Response
@@ -28,8 +28,11 @@ TX_POWER.value(1)  # Set TX_POWER pin to HIGH to provide 3.3V power
 transmit = TX(pin(), 'washRoomCeilFan') #currently pin 14 (set in tx/get_pin.py)
     #Washer door mag sensor
 washerDoor = machine.Pin(16, machine.Pin.IN)
-#Threads kill pin.  Pull this circuit if you can't connect since it starts main.py
+    #Threads kill pin.  Pull this circuit if you can't connect since it starts main.py
 threads_pin = machine.Pin(19, machine.Pin.IN)
+#lcd display
+lcd = LCD()
+lcd.clear()
 #variables
 url = 'http://192.168.86.33:5000/api'
 rtc = machine.RTC()
@@ -70,15 +73,26 @@ def connect():
     wlan.config(hostname='picowashroom')
     wlan.active(True)
     wlan.connect(ssid, passwd)
+    iter = 1
     while wlan.isconnected() == False:
-        print('Waiting for connection...')
+        lcd.clear()
+        string = f'Waiting for \n'
+        lcd.message(string)
+        string = f'connection...{iter}'
+        lcd.message(string)
         sleep(1)
+        iter += 1
     ip = wlan.ifconfig()[0]
+    lcd.clear()
+    string = f'{network.hostname()} \n'
+    lcd.message(string)
+    string = f'{ip}'
+    lcd.message(string)
     print(f'{network.hostname()} is connected on {ip}')
     return ip
 
 def fanOn():
-    test = f'{time.localtime()[0]}/{months[time.localtime()[1]]}/{time.localtime()[2]:02d} {time.localtime()[3]:02d}:{time.localtime()[4]:02d}:{time.localtime()[5]:02d} Fan On'
+    test = f'{utcToLocal(datetime)} Fan On'
     with open('washroom.log', 'a') as fw:
         fw.write(test)
         fw.write('\n')
@@ -88,7 +102,7 @@ def fanOn():
     utime.sleep(0.5)
 
 def fanOff():
-    test = f'{time.localtime()[0]}/{months[time.localtime()[1]]}/{time.localtime()[2]:02d} {time.localtime()[3]:02d}:{time.localtime()[4]:02d}:{time.localtime()[5]:02d} Fan Off'
+    test = f'{utcToLocal(datetime)} Fan Off'
     with open('washroom.log', 'a') as fw:
         fw.write(test)
         fw.write('\n')
@@ -97,9 +111,31 @@ def fanOff():
     transmit('fanOnOff')
     utime.sleep(0.5)
 
-def lightToggle():
+def lightToggle(state):
+    before_adc_value = adc.read_u16()
     transmit('lightTogg')
     utime.sleep(0.2)
+    after_adc_value = adc.read_u16()
+    print(f"Before: {before_adc_value}, After: {after_adc_value}")
+    if before_adc_value > after_adc_value and state == 'Off':
+        transmit('lightTogg')
+        utime.sleep(0.2)
+    elif after_adc_value > before_adc_value and state == 'On':
+        transmit('lightTogg')
+        utime.sleep(0.2)
+    
+
+def utcToLocal(type):
+    global localUtcOffset
+    global months
+    localTime = time.localtime(time.time() + localUtcOffset)
+    print(localTime[0])
+    if type == 'time':
+        return f'{localTime[3]:02d}:{localTime[4]:02d}:{localTime[5]:02d}'
+    elif type == 'date':
+        return f'{localTime[0]}/{months[localTime[1]]}/{localTime[2]:02d}'
+    else:
+        return f'{localTime[0]}/{months[localTime[1]]}/{localTime[2]:02d} {localTime[3]:02d}:{localTime[4]:02d}:{localTime[5]:02d}'
 
 def core1():
     #variables
@@ -107,7 +143,9 @@ def core1():
     global rtc
     global run_threads
     global threads_pin
+    global lcd
     led_on = False
+    #lcd.clear()
     last_data_pin_change = 0
     countdown_duration = 30
     last_closed_door_change = 0
@@ -116,22 +154,23 @@ def core1():
     door_opened_threshold = 1200 # 20 minutes
     fan_stage = 1 #1 will mean nothing, 2 will mean 5 minutes door closed counting, 3 will mean waiting for door to open, 4 for 20 minutes open door countdown
     #Toggle light to initiate whether it is on or off
-    before_adc_value = adc.read_u16()
-    lightToggle()
+    #before_adc_value = adc.read_u16()
+    lightToggle('Off')
     #utime.sleep(0.2)
-    after_adc_value = adc.read_u16()
-    print(f"Before: {before_adc_value}, After: {after_adc_value}")
-    if before_adc_value > after_adc_value:
-        lightToggle()
+    #after_adc_value = adc.read_u16()
+    #print(f"Before: {before_adc_value}, After: {after_adc_value}")
+    #if before_adc_value > after_adc_value:
+    #    lightToggle()
         #utime.sleep(0.2)
     print('Light initiated...')
     doorCheckCount = 25
     # Main loop
-    while while threads_pin.value() == 1:
+    while threads_pin.value() == 1:
         if not run_threads:
             print('run thread interrupt')
             break
         if doorCheckCount == 25:
+            #print(f'Washer door is {washerDoor.value()}')
             if washerDoor.value() == 1:
                 last_opened_door_change = 0
                 if fan_stage == 1:
@@ -140,43 +179,61 @@ def core1():
                     fan_stage = 2
                 elif fan_stage == 2:
                     if (utime.time() - last_closed_door_change) >= door_closed_threshold:
-                        print('Door closed for over 5 minutes, waiting for door open')
+                        lcd.clear()
+                        string = f'Door closed, \n wait for open..{fan_stage}'
+                        lcd.message(string)
+                        #print('Door closed for over 5 minutes, waiting for door open')
                         fanOff()
                         last_closed_door_change = 0
                         fan_stage = 3
                     else:
                         timer = door_closed_threshold - (utime.time() - last_closed_door_change)
-                        print(f'Door closed countdown: {timer}')
+                        #print(f'Door closed countdown: {timer}')
+                        lcd.clear()
+                        string = f'Door closed \n countdown: {timer}'
+                        lcd.message(string)
                 elif fan_stage == 4:
                     last_opened_door_change = 0
                     fan_stage = 3
-                    print('Door was closed before 20 minutes of being open, resetting')
+                    lcd.clear()
+                    string = f'Door closed, fan\nstage reset to {fan_stage}'
+                    lcd.message(string)
+                    #print('Door was closed before 20 minutes of being open, resetting')
             if washerDoor.value() == 0:
                 last_closed_door_change = 0
                 if fan_stage == 2:
                     #this means the door was NOT closed for 5+ minutes, reset it
                     fan_stage = 1
-                    print('Door opened before closed threshold, resetting')
+                    lcd.clear()
+                    string = f'Door opened, fan \nstage reset to {fan_stage}'
+                    lcd.message(string)
+                    #print('Door opened before closed threshold, resetting')
                 elif fan_stage == 3:
                     last_opened_door_change = utime.time()
                     fan_stage = 4
                 elif fan_stage == 4:
                     if (utime.time() - last_opened_door_change) >= door_opened_threshold:
                         fanOn()
+                        lcd.clear()
+                        string = f'{utcToLocal(time)}\nFan Started'
+                        lcd.message(string)
                         last_opened_door_change = 0
                         fan_stage = 1 #Reset fan stage once you start cieling fan
                     else:
                         timer = door_opened_threshold - (utime.time() - last_opened_door_change)
-                        print(f'Door opened countdown: {timer}')
+                        lcd.clear()
+                        string = f'Door opened \n countdown: {timer}'
+                        lcd.message(string)
+                        #print(f'Door opened countdown: {timer}')
             doorCheckCount = 1
         # Read data from DATA_PIN
         data = DATA_PIN.value()
         #print(adc.read_u16())
         if data == 1:
             if not led_on:
-                test = f'{time.localtime()[0]}/{months[time.localtime()[1]]}/{time.localtime()[2]:02d} {time.localtime()[3]:02d}:{time.localtime()[4]:02d}:{time.localtime()[5]:02d} Light On'
+                test = f'{utcToLocal(datetime)} Light On'
                 led_on = True
-                lightToggle()
+                lightToggle('On')
                 #utime.sleep(0.2)
                 with open('washroom.log', 'a') as fw:
                     fw.write(test)
@@ -189,11 +246,11 @@ def core1():
         else:
             if led_on and (utime.time() - last_data_pin_change) >= countdown_duration:
                 led_on = False
-                lightToggle()
+                lightToggle('Off')
                 #utime.sleep(0.2)
                 print("LED turned off.")
-            elif led_on:
-                print(countdown_duration - (utime.time() - last_data_pin_change))
+            #elif led_on:
+            #    print(countdown_duration - (utime.time() - last_data_pin_change))
 
         # Read the ADC value
         #adc_value = adc.read_u16()
@@ -221,6 +278,7 @@ def webpage(temperature, state):
     return str(html)
 
 app = Microdot()
+
 Response.default_content_type = 'text/html'
 
 @app.route('/')
@@ -239,6 +297,9 @@ def index(request):
         while lastEntry <= firstEntry:
             washLogs.append(examp[firstEntry-1])
             firstEntry = firstEntry - 1
+    '''with open('washroom.log','r') as f:
+        for line in f.readlines():
+            washLogs.append(line)'''
     return render_template('home.html', logs=washLogs)
 
 @app.route('/orders', methods=['GET'])
@@ -257,7 +318,7 @@ def washRoomLog(request):
             return 'done'
         else:
             return '404'
-        
+
 @app.route('/shutdown')
 def shutdown(request):
     request.app.shutdown()
@@ -266,7 +327,9 @@ def shutdown(request):
 try:
     connect()
     ntptime.settime()
-    print(time.localtime())
+    response = urequests.get('https://timeapi.io/api/TimeZone/zone?timeZone=America/Denver')
+    localUtcOffset = response.json()['currentUtcOffset']['seconds']
+    print(utcToLocal(datetime))
 except KeyboardInterrupt:
     run_threads = False
 
@@ -279,6 +342,4 @@ if __name__ == '__main__':
         app.run(port=80)
     except KeyboardInterrupt:
         run_threads = False
-#except KeyboardInterrupt:
-#    machine.reset()
 
