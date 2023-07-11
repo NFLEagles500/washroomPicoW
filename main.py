fc96a8b0d446ace3519f0456f8a750577deae4ca
NFLEagles500 <NFLEagles500@gmail.com>
2023-07-10 22:57:17 -0600

    Update main.py
    removed threads_pin requirement

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ transmit = TX(pin(), 'washRoomCeilFan') #currently pin 14 (set in tx/get_pin.py)
     #Washer door mag sensor
 washerDoor = machine.Pin(16, machine.Pin.IN)
     #Threads kill pin.  Pull this circuit if you can't connect since it starts main.py
-threads_pin = machine.Pin(19, machine.Pin.IN)
+#threads_pin = machine.Pin(19, machine.Pin.IN)
 
 #variables
 url = 'http://192.168.86.33:5000/api'
@@ -180,7 +180,7 @@ def core1():
     global months
     global rtc
     global run_threads
-    global threads_pin
+    #global threads_pin
     led_on = False
     last_data_pin_change = 0
     countdown_duration = 30
@@ -201,83 +201,83 @@ def core1():
     print('Light initiated...')
     doorCheckCount = 25
     # Main loop
-    while threads_pin.value() == 1:
-        if not run_threads:
-            print('run thread interrupt')
-            break
-        if doorCheckCount == 25:
-            #print(f'Washer door is {washerDoor.value()}')
-            if washerDoor.value() == 1:
-                last_opened_door_change = 0
-                if fan_stage == 1:
-                    print('Door closed, starting closed timer')
-                    last_closed_door_change = utime.time()
-                    fan_stage = 2
-                elif fan_stage == 2:
-                    if (utime.time() - last_closed_door_change) >= door_closed_threshold:
-                        #print('Door closed for over 5 minutes, waiting for door open')
-                        fanOff()
-                        last_closed_door_change = 0
-                        fan_stage = 3
-                    else:
-                        timer = door_closed_threshold - (utime.time() - last_closed_door_change)
-                        #print(f'Door closed countdown: {timer}')
-                elif fan_stage == 4:
-                    last_opened_door_change = 0
+    #while threads_pin.value() == 1:
+    #if not run_threads:
+    #    print('run thread interrupt')
+    #    break
+    if doorCheckCount == 25:
+        #print(f'Washer door is {washerDoor.value()}')
+        if washerDoor.value() == 1:
+            last_opened_door_change = 0
+            if fan_stage == 1:
+                print('Door closed, starting closed timer')
+                last_closed_door_change = utime.time()
+                fan_stage = 2
+            elif fan_stage == 2:
+                if (utime.time() - last_closed_door_change) >= door_closed_threshold:
+                    #print('Door closed for over 5 minutes, waiting for door open')
+                    fanOff()
+                    last_closed_door_change = 0
                     fan_stage = 3
-                    #print('Door was closed before 20 minutes of being open, resetting')
-            if washerDoor.value() == 0:
-                last_closed_door_change = 0
-                if fan_stage == 2:
-                    #this means the door was NOT closed for 5+ minutes, reset it
-                    fan_stage = 1
-                    #print('Door opened before closed threshold, resetting')
-                elif fan_stage == 3:
-                    last_opened_door_change = utime.time()
-                    fan_stage = 4
-                elif fan_stage == 4:
-                    if (utime.time() - last_opened_door_change) >= door_opened_threshold:
-                        fanOn()
-                        last_opened_door_change = 0
-                        fan_stage = 1 #Reset fan stage once you start cieling fan
-                    else:
-                        timer = door_opened_threshold - (utime.time() - last_opened_door_change)
-                        #print(f'Door opened countdown: {timer}')
-            doorCheckCount = 1
-        # Read data from DATA_PIN
-        data = DATA_PIN.value()
-        #print(adc.read_u16())
-        if data == 1:
-            if not led_on:
-                test = f'{utcToLocal('datetime')} Light On'
-                led_on = True
-                lightToggle('On')
-                #utime.sleep(0.2)
-                with open('washroom.log', 'a') as fw:
-                    fw.write(test)
-                    fw.write('\n')
-                print("Light turned on.")
-            #led_on = True
-            #LED_PIN.value(1)
-            last_data_pin_change = utime.time()
-            
-        else:
-            if led_on and (utime.time() - last_data_pin_change) >= countdown_duration:
-                led_on = False
-                lightToggle('Off')
-                #utime.sleep(0.2)
-                print("Light turned off.")
-            #elif led_on:
-            #    print(countdown_duration - (utime.time() - last_data_pin_change))
-
-        # Read the ADC value
-        #adc_value = adc.read_u16()
-
-        doorCheckCount += 1
-        if utime.localtime()[3] == 2 and utime.localtime()[4] == 0 and utime.localtime()[5] == 0:
-            logCleanup()
-        utime.sleep(0.2)  # Delay for 0.1 seconds
+                else:
+                    timer = door_closed_threshold - (utime.time() - last_closed_door_change)
+                    #print(f'Door closed countdown: {timer}')
+            elif fan_stage == 4:
+                last_opened_door_change = 0
+                fan_stage = 3
+                #print('Door was closed before 20 minutes of being open, resetting')
+        if washerDoor.value() == 0:
+            last_closed_door_change = 0
+            if fan_stage == 2:
+                #this means the door was NOT closed for 5+ minutes, reset it
+                fan_stage = 1
+                #print('Door opened before closed threshold, resetting')
+            elif fan_stage == 3:
+                last_opened_door_change = utime.time()
+                fan_stage = 4
+            elif fan_stage == 4:
+                if (utime.time() - last_opened_door_change) >= door_opened_threshold:
+                    fanOn()
+                    last_opened_door_change = 0
+                    fan_stage = 1 #Reset fan stage once you start cieling fan
+                else:
+                    timer = door_opened_threshold - (utime.time() - last_opened_door_change)
+                    #print(f'Door opened countdown: {timer}')
+        doorCheckCount = 1
+    # Read data from DATA_PIN
+    data = DATA_PIN.value()
+    #print(adc.read_u16())
+    if data == 1:
+        if not led_on:
+            test = f'{utcToLocal('datetime')} Light On'
+            led_on = True
+            lightToggle('On')
+            #utime.sleep(0.2)
+            with open('washroom.log', 'a') as fw:
+                fw.write(test)
+                fw.write('\n')
+            print("Light turned on.")
+        #led_on = True
+        #LED_PIN.value(1)
+        last_data_pin_change = utime.time()
         
+    else:
+        if led_on and (utime.time() - last_data_pin_change) >= countdown_duration:
+            led_on = False
+            lightToggle('Off')
+            #utime.sleep(0.2)
+            print("Light turned off.")
+        #elif led_on:
+        #    print(countdown_duration - (utime.time() - last_data_pin_change))
+
+    # Read the ADC value
+    #adc_value = adc.read_u16()
+
+    doorCheckCount += 1
+    if utime.localtime()[3] == 2 and utime.localtime()[4] == 0 and utime.localtime()[5] == 0:
+        logCleanup()
+    utime.sleep(0.2)  # Delay for 0.1 seconds
+    
 
 
 def webpage(temperature, state):
